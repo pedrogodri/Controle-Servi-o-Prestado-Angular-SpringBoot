6edb390df97fe8b4515cbe92a379ec89594cef50
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-04 20:34:40 -0300

    Projeto Spring

---
+++ clientes-back/src/main/java/com/br/projeto/clientesback/ClientesBackApplication.java
@@ -0,0 +1,13 @@
+package com.br.projeto.clientesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesBackApplication.class, args);
+	}
+
+}
---
+++ clientes-back/src/test/java/com/br/projeto/clientesback/ClientesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projeto.clientesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
