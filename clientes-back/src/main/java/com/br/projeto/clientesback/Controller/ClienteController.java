ebac4ac84a57e7c84c438acd43f8eca45d66d7a8
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-05 09:31:53 -0300

    Metodo de editar do cliente

--- clientes-back/src/main/java/com/br/projeto/clientesback/Controller/ClienteController.java
+++ clientes-back/src/main/java/com/br/projeto/clientesback/Controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,6 +46,20 @@ public class ClienteController {
         .findById(id)
         .map(cliente -> {
             repository.delete(cliente);
+            return Void.TYPE;
+        })
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void editar(@PathVariable Integer id, @RequestBody Cliente clienteAtt) {
+        repository
+        .findById(id)
+        .map(cliente -> {
+            cliente.setNome(clienteAtt.getNome());
+            cliente.setCpf(clienteAtt.getCpf());
+            repository.save(clienteAtt);
             return Void.TYPE;
         })
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
