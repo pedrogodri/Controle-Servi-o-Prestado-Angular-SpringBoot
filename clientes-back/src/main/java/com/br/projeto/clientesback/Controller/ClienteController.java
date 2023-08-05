0dafd8aa0cf5fe0d7f24390a96a2be971d7cc5cd
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-05 09:06:28 -0300

    Metodo delete

--- clientes-back/src/main/java/com/br/projeto/clientesback/Controller/ClienteController.java
+++ clientes-back/src/main/java/com/br/projeto/clientesback/Controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.br.projeto.clientesback.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,17 @@ public class ClienteController {
     @GetMapping("{id}")
     public Cliente BuscarId(@PathVariable Integer id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+        repository
+        .findById(id)
+        .map(cliente -> {
+            repository.delete(cliente);
+            return Void.TYPE;
+        })
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
