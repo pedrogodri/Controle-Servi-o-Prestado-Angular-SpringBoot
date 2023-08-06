974e51b17ba08f8ab6c97395e9160d258d2a6d38
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-06 09:44:41 -0300

    feat: componente cliente

--- clientes-front/src/app/app.module.ts
+++ clientes-front/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
