b271136f1e9643cc123d822c954ac5663de55d01
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-06 22:27:51 -0300

    template front finalizado

---
+++ clientes-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { TemplateModule } from './template/template.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TemplateModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
