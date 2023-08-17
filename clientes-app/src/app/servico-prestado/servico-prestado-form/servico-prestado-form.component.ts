1cfb5bfc7cf33672cbb7dbc61096e9f5fb4fff0a
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2023-08-17 09:01:24 -0300

    Feat: forms de servico

--- clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/model/cliente';
+import { ClientesService } from '../../service/clientes.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
   styleUrls: ['./servico-prestado-form.component.css']
 })
-export class ServicoPrestadoFormComponent {
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  clientes: Cliente[] = [];
+
+  constructor(
+    private clienteService: ClientesService
+  ) { }
+
+  ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(
+      response => {
+        this.clientes = response;
+      }
+    )
+  }
+
+  onSubmit() {
+    
+  }
 
 }
