5455ce9b7d18474b1e61849aedd3015939a7efd5
Pedro Henrique Godri <pedrogodri@gmail.com>
2023-08-19 16:40:54 -0300

    feat: tela de login

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -10,17 +10,21 @@ import { HomeComponent } from './home/home.component';
 import { ClientesModule } from './clientes/clientes.module';
 import { ClientesService } from './service/clientes.service';
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
+    FormsModule,
     ClientesModule,
     ServicoPrestadoModule
   ],
